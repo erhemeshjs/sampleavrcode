8020d1606bc5768c00ae978dc3bceceb55353f30
robert@einsle.de <robert@einsle.de@bedfcf92-b74c-0410-ab04-2fd4caf951fe>
2008-05-09 06:49:00 +0000

    Dokumentation der Sourcen

--- trunk/samples/c/09-timer-interrupt/09-timer-interrupt.c
+++ trunk/samples/c/09-timer-interrupt/09-timer-interrupt.c
@@ -1,9 +1,12 @@
 /*
  * Beispielprogramm 09-timer-interrupt.c
+ *
+ * Beispielprogramm, das den internen Timer im AVR nutzt um eine LED
+ * an PD5 blinken zu lassen.
+ *
  * (c) Robert Einsle <robert@einsle.de>
  * (c) Michael Hartmann <michael@speicherleck.de>
- * V 0.1, 01-05-2008
- * ATtiny 2313
+ * V 1.0, 09.05.2008
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
