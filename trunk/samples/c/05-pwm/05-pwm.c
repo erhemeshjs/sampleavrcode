a773aa741909a454ae352747cb8487d39657ed53
robert@einsle.de <robert@einsle.de@bedfcf92-b74c-0410-ab04-2fd4caf951fe>
2008-05-12 11:27:42 +0000

    Anpassung an Attiny 2313

--- trunk/samples/c/05-pwm/05-pwm.c
+++ trunk/samples/c/05-pwm/05-pwm.c
@@ -25,17 +25,31 @@
 
 int main(void) {
 
-  DDRD |= (1<<PD4)|(1<<PD5);
-  TCCR1A = (1<<COM1A1)|(1<<COM1B1)|(1<<WGM10); 
-  TCCR1B = (1<<CS11); 
+  // PIN D5 auf Ausgang
+  DDRD |= (1 << PD5);
+
+  TCCR0A = (1 << WGM00) | (1 << WGM01);  // Fast PWM Mode
+  TCCR0A |= (1 << COM0B1) | (1 << COM0B0);  // Set OC0B on Compare Match,
+                                            // clear OC0B at TOP
+  TCCR0B = (1 << CS00);  // Prescaler = CPU-Clock
+
+  OCR0B = 0;	
+
+//  TCCR0A = (1 << WGM00);  // Phase correct PWM Mode
+//  TCCR0B = (1 << WGM02);  // Phase correct PWM Mode
+//  TCCR0B |= (1 << CS01);
+//  TCCR0A |= (1 << COM0B0) | (1 << COM0B1);  // 
+
+//  TCCR0A = (1<<COM0A1)|(1<<COM0B1)|(1<<WGM10); 
+//  TCCR0B = (1<<CS11); 
   int i;
 
   // Verarbeitungsschleife
   while(1) {
     for (i = 0; i < 255; i=i+1)
       {
-        OCR1A = i;
-        _delay_ms(10);
+        OCR0B = i;
+        _delay_ms(50);
       }
   } 
 
