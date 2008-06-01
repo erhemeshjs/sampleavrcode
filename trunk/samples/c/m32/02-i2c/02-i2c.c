51ad36817692ecc2b2a4d840c4ae406f46a3f72c
robert@einsle.de <robert@einsle.de@bedfcf92-b74c-0410-ab04-2fd4caf951fe>
2008-06-01 10:04:02 +0000

    Aktualisierung der Routinen zum Schreiben auf dem I2C Bus

--- trunk/samples/c/m32/02-i2c/02-i2c.c
+++ trunk/samples/c/m32/02-i2c/02-i2c.c
@@ -26,50 +26,65 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <util/twi.h>
+#include "uart.h"
 
-void error(void);
+#define	TRUE	    1
+#define	FALSE	    0
+
+#define I2C_ADDR    0xA0
+
+#define I2C_READ    1
+#define I2C_WRITE   0
+
+void twi_init(void);
+char twi_start(void);
+char twi_set_address(char data);
+char twi_send(char data);
+void twi_stop(void);
+void twi_wait(void);
+void twi_show_sr(void);
 
 int main(void)
 {
     DDRD = (1 << PD5);
+
+    // Init des UArts
+    uart_init();
+
+    printf("Hallo Welt\r\n");
   
     // Init des TWI-Bus
-    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-  
-    // Warten bis TW Start transmitted
-    while (!(TWCR & (1 << TWINT)))
-      ;
-  
-    // Ueberpruefung ob START-Condition
-	if ((TWSR & 0xF8) != TW_START)
-	  error();
-	  
-	// Setzen der Client-Address
-	TWDR = 0xA1; // Client Address
-	TWCR = (1 << TWINT) | (1 << TWEN);
-	
-    // Warten bis TW Start transmitted
-    while (!(TWCR & (1 << TWINT)))
-      ;
-  
-    // Ueberpruefung ob START-Condition
-	if ((TWSR & 0xF8) != TW_START)
-	  error();
-	  
-	// Schreiben der Daten an den Client
-	TWDR = 0xFF;
-	TWCR = (1 << TWINT) | (1 << TWEN);
-	
-    // Warten bis TW Start transmitted
-    while (!(TWCR & (1 << TWINT)))
-      ;
-  
-    // Ueberpruefung ob START-Condition
-	if ((TWSR & 0xF8) != TW_START)
-	  error();
+    twi_init();
 
-    // Senden von Stop
-    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);	
+    if (twi_start())
+    {
+      if (twi_set_address(I2C_ADDR + I2C_WRITE))
+      {
+        if (twi_send(0xFF))
+	{
+	  uart_puts("Daten erfolgreich geschrieben\r\n");
+	} else {
+	  uart_puts("Daten nicht erfolgreich\r\n");
+	}
+      }
+      else
+      {
+        uart_puts("Adresse nicht erfolgreich uebertragen\r\n");
+      }
+      twi_stop;
+    } else {
+      uart_puts("Start konnte nicht gesendet werden!\r\n");
+      twi_stop();
+    }
+
+    // Setzen der Client-Adresse
+    //twi_send(0xA1);
+
+    // Senden der Daten an den Chip
+    //twi_send(0xFF);
+
+    // Stop des TWI-Busses
+    //twi_stop();
 
     // Verarbeitungsschleife
     while(1)
@@ -78,6 +93,88 @@ int main(void)
     return 0;
 }
 
-void error(void)
+void twi_init(void)
 {
+    // Init des TWI-Bus
+    uart_puts("Init des TWI-Bus\r\n");
+    //TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+    TWCR = (1 << TWEN);
+    TWSR = 0x00;
+
+    // Setzen der Bitrate
+    TWBR = 0x48;
+}
+
+char twi_start(void)
+{
+    // Senden der Start-Condition
+    TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+
+    twi_wait();
+
+    twi_show_sr();
+
+    // Ueberpruefung ob START-Condition
+    if (TWSR & 0xF8  != TW_START)
+      return FALSE;
+    return TRUE;
+}
+
+void twi_wait(void) 
+{
+    while (!(TWCR & (1 << TWINT)))
+      ;
+}
+
+char twi_set_address(char data)
+{
+    // Setzen der Daten
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+	
+    // Warten bis TW Start transmitted
+    while (!(TWCR & (1 << TWINT)))
+      ;
+  
+    twi_show_sr();
+    // Ueberpruefung ob START-Condition
+    if ((TWSR & 0xF8) != TW_MT_SLA_ACK)
+    {
+      printf("TW_START: %#x\r\n", TW_MT_SLA_ACK);
+      return FALSE;
+    }
+    return TRUE;
+}
+
+char twi_send(char data)
+{
+    // Setzen der Daten
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+	
+    // Warten bis TW Start transmitted
+    while (!(TWCR & (1 << TWINT)))
+      ;
+  
+    twi_show_sr();
+    // Ueberpruefung ob START-Condition
+    if ((TWSR & 0xF8) != TW_MT_DATA_ACK)
+    {
+      printf("TW_MT_DATA_ACK: %#x\r\n", TW_MT_DATA_ACK);
+      return FALSE;
+    }
+    return TRUE;
+}
+
+void twi_stop(void)
+{
+    // Senden von Stop
+    uart_puts("Stop der Übertragung\r\n");
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);	
+}
+
+void twi_show_sr(void)
+{
+    // Anzeige Inhalt des Status-Registers
+    printf("TWSR: %#x\r\n", TWSR);
 }
